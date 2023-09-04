55898d1b1a2a031942ea638fd5f6c208bdb644a9
Ernesto Lopez <ernestodaniellopez504@gmail.com>
2023-09-03 21:59:16 -0400

    Avances con el frontEnd
    Se prepara el context, el layout principal; los componentes para usuarios, modal de agregar y tareas; un helper en /api para las peticiones y un hook para usar el context

---
+++ frontend/src/components/DropDownTaskList.jsx
@@ -0,0 +1,16 @@
+
+const DropDownTaskList = ({type}) => {
+  return (
+    <div className="person__specifiedTasks">
+      <h3 className="person__tasksStatus">Tasks</h3>
+      <ul className="person__taksList">
+        <li className="person__taskItem">Task1</li>
+        <li className="person__taskItem">Task2</li>
+        <li className="person__taskItem">Task3</li>
+        <li className="person__taskItem">Task4</li>
+      </ul>
+    </div>
+  )
+}
+  
+export default DropDownTaskList
