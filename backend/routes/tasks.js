6dab67901a271596e7393dbba2dde3260a429304
Javier Villalba <javier.villalba@cast.com.py>
2023-09-11 00:44:09 -0400

    feat: add task query endpoints

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -100,6 +100,7 @@ router.delete('/:id', getTask, async (req, res, next) => {
 
 // middleware function to get resource by id
 async function getTask(req, res, next) {
+    let task;
     try {
         task = await Task.findById(req.params.id);
         if (task == null) {
@@ -112,5 +113,91 @@ async function getTask(req, res, next) {
     res.task = task;
     next();
 }
+
+// get tastks by user id
+// FIXME cuando se envía la petición http://localhost:3000/tasks/user/user/ se obtiene el error
+// message": "Cast to ObjectId failed for value \"user\" (type string) at path \"_id\" for model \"Task\""
+router.get('/user/:id', async (req, res, next) => {
+    try {
+        //validar si se recibe el id del usuario
+        if (req.params.id == null || req.params.id == "") {
+            return res.status(404).json({ message: 'Cannot find user id' });
+        }
+        const tasks = await Task.find({ userId: req.params.id });
+        res.json(tasks);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// get tastks by category id
+router.get('/category/:id', async (req, res, next) => {
+    try {
+        const tasks = await Task.find({ categoryId: req.params.id });
+        res.json(tasks);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// get tastks by status
+router.get('/status/:status', async (req, res, next) => {
+    try {
+        const tasks = await Task.find({ status: req.params.status });
+        res.json(tasks);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// get tastks by priority
+router.get('/priority/:priority', async (req, res, next) => {
+    try {
+        const tasks = await Task.find({ priority: req.params.priority });
+        res.json(tasks);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// post query tasks
+router.post('/query', async (req, res, next) => {
+    try {
+        const query = Task.find();
+        query.setOptions({ lean: true });
+        query.collection(Task.collection);
+        if (req.body.name != null) {
+            query.where('name').regex(new RegExp(req.body.name, 'i'));
+        }
+        if (req.body.description != null) {
+            query.where('description').regex(new RegExp(req.body.description, 'i'));
+        }
+        if (req.body.status != null) {
+            query.where('status').equals(req.body.status);
+        }
+        if (req.body.priority != null) {
+            query.where('priority').equals(req.body.priority);
+        }
+        if (req.body.dueDate != null) {
+            query.where('dueDate').equals(req.body.dueDate);
+        }
+        if (req.body.userId != null) {
+            query.where('userId').equals(req.body.userId);
+        }
+        if (req.body.categoryId != null) {
+            query.where('categoryId').equals(req.body.categoryId);
+        }
+        if (req.body.createdBy != null) {
+            query.where('createdBy').equals(req.body.createdBy);
+        }
+        if (req.body.updatedBy != null) {
+            query.where('updatedBy').equals(req.body.updatedBy);
+        }
+        const tasks = await query.exec();
+        res.json(tasks);
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports = router;
